262e92cc9acac5b5c6193a139e8e22e38275f35c
binarylover <whitedevil.hamd@gmail.com>
2023-12-23 21:06:52 +0530

    first commit

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 1443
+let accountEmail = "lucky@gmail.com"
+var accountPawword = "12345" 
+accounCity = "jaipur" // also possible
+let nameOfBook
+
+// accountId = 2    // Not allowed
+
+accountEmail = "lu@co.in"
+accountPawword = "23434"
+accounCity = "Delhi"
+/*
+prefer not to use var
+because of issue in block scope and funcitonal scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPawword, accounCity, nameOfBook]) // .table print all variable in tabular form
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("helow") // we are using notejs, not browser
+
+console.log("Lucky");
+
+
+let name = "lucky"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => for uniqueness
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof(undefined)); // undefined
