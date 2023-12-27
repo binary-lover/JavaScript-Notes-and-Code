73f750c2627347a878d22238cac9e28f4882a466
binarylover <whitedevil.hamd@gmail.com>
2023-12-27 11:16:01 +0530

    numbs and string added

---
+++ 01_basics/06_nums_&_maths.js
@@ -0,0 +1,32 @@
+const score = 400;
+
+const balance = new Number(23000.658)
+
+// console.log(score );
+// console.log(balance);
+
+// console.log(balance.toString().length); 
+// console.log(balance.toFixed(2));
+
+const  otherNumber = 34.596
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// console.log(Number.MAX_VALUE); // gives max range of Numbers in javascript
+
+
+
+// **************** Maths ***************
+
+console.log(Math);
+console.log(Math.abs(-3));  // Work like Mod |x|
+console.log(Math.round(4.3)); // .round() is a function
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+console.log(Math.min(4,3,2,6,5));
+console.log(Math.max(4,3,2,6,5));
+
+console.log((Math.random()*10) + 1);
