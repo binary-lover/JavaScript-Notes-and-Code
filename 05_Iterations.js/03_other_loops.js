ade398ff3df5b561b8b1bbc95f2f1a7be1921b7c
binarylover <whitedevil.hamd@gmail.com>
2024-01-05 22:10:20 +0530

    filtered

--- 05_Iterations.js/03_other_loops.js
+++ 05_Iterations.js/03_other_loops.js
@@ -23,7 +23,7 @@ map.set('USA',"United states of america")
 
 // console.log(map.get('IN'));
 
-for (const key of map) {  // each entry will be store in array
+for (const key of map) {  // each key entry will be store in array
     // console.log(key);
 }
 
@@ -40,19 +40,19 @@ const myObject = {
  
 // making object iterable we can use for in loop
 for (const key in myObject) {
-    console.log(`${key} :- ${myObject[key]}`);
+    // console.log(`${key} :- ${myObject[key]}`);
 }
 
 // useing for in iterating an array
 const myArray = ["js","c","cpp","py"]
 for (const key in myArray) {
-    // console.log(myArray[key]);
+    console.log(myArray[key]);
     // process.stdout.write(myArray[key]+ " ") // this just print all array elemenst in one line
 }
 // for of loop print direct values of array but for in print index and not the actual values
 
 // iterating maps using for in
 for (const key in map) {
-    console.log(key); // no output as maps are not iterable
+    // console.log(key); // no output as maps are not iterable
 }
 
